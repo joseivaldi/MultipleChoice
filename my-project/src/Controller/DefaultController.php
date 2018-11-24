6e234bc18a8fa11ce039b06e34b4e31f6723408b
joseivaldi <40646985+joseivaldi@users.noreply.github.com>
2018-11-24 16:52:23 -0300

    Comenzando a trabajar. Intentando acomodar las preguntas en la pagina localhost

---
+++ my-project/src/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Yaml\Yaml;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/default", name="default")
+     */
+    public function index()
+    {
+        $Archivo_parseado = Yaml::parseFile(__DIR__.'/preguntas.yml');
+        $Preguntas = $Archivo_parseado["preguntas"];
+
+        //dump($Preguntas);
+
+        return $this->render('default/index.html.twig', ["preguntas" => $Pregunta
+        ]);
+    }
+}

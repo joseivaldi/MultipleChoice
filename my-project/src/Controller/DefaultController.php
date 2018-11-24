0018429403fbefd4eb1a06f52b6e8273da084802
joseivaldi <40646985+joseivaldi@users.noreply.github.com>
2018-11-24 18:14:27 -0300

    Agregando rtas
    Mezclando las rtas, imprimirlas y darle formato

--- my-project/src/Controller/DefaultController.php
+++ my-project/src/Controller/DefaultController.php
@@ -13,12 +13,20 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
-        $Archivo_parseado = Yaml::parseFile(__DIR__.'/preguntas.yml');
-        $Preguntas = $Archivo_parseado["preguntas"];
-        shuffle($Preguntas);   
+        $Archivo_parseado = Yaml::parseFile(__DIR__.'/preguntas.yml');  #Parciando archivo
+        $Preguntas = $Archivo_parseado["preguntas"];        #tiene un elemento que tiene las 25 preguntas
+        shuffle($Preguntas);                                #Mezclamos las pregs
 
+        $Respuestas_mezcladas= [];                      #Mezclar las rtas
+
+        foreach($Preguntas as $pregunta){
+            $mezcla_preguntas = array_merge($pregunta["respuestas_correctas"], $pregunta["respuestas_incorrectas"]);
+            shuffle($mezcla_preguntas); 
+            $Respuestas_mezcladas[]=$mezcla_preguntas;
+
+        }
         //dump($Preguntas);
 
-        return $this->render('default/index.html.twig', ["preguntas" => $Preguntas]);
+        return $this->render('default/index.html.twig', ["preguntas" => $Preguntas, "Respuestas_mezcladas" => $Respuestas_mezcladas]);
     }
 }

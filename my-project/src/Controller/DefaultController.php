1bd5cff14bf86726df39bc9d3eda9e570ca49d2e
joseivaldi <40646985+joseivaldi@users.noreply.github.com>
2018-11-25 18:01:28 -0300

    pasando en limpio y corrigiendo errores

--- my-project/src/Controller/DefaultController.php
+++ my-project/src/Controller/DefaultController.php
@@ -11,67 +11,63 @@ class DefaultController extends AbstractController
     /**
      * @Route("/default", name="default")
      */
-    public function Parsear_Archivo ($Archivo){
 
-        $Archivo_parseado = Yaml :: parseFile(__DIR__.$Archivo);
+    public function Parsear_Archivo($Archivo){
 
-        return $Archivo_parseado;
+		$Archivo_parseado = Yaml::parseFile(__DIR__.$Archivo);
+
+    	return $Archivo_parseado;
 
     }
 
-    public function Mezclar_Preguntas(){
+    public function Mezaclar_Preguntas(){
 
-        $Preguntas = $this->Parsear_Archivo("/preguntas.yml");
-        $Preguntas = $Preguntas["preguntas"];
-        shuffle($Preguntas);
+    	$Preguntas = $this->Parsear_Archivo("/preguntas.yml");
+    	$Preguntas = $Preguntas["preguntas"];
+    	shuffle($Preguntas);
 
-        $Respuestas_mezcladas = []
+    	$Respuestas_mezcladas = [];
+    	
+    	foreach ($Preguntas as $pregunta) {
+    		$mezcla_preguntas = array_merge($pregunta["respuestas_correctas"],$pregunta["respuestas_incorrectas"]);
+    		shuffle($mezcla_preguntas);
+    		$Respuestas_mezcladas[]=$mezcla_preguntas;
+    	}
 
+    	$Preguntas_de_Ultimo_Examen = Yaml::dump($Preguntas);
 
-        //Este for me toma una pregunta de la lista de preguntas
-        foreach ($Preguntas  as $pregunta){     
-            $mezcla_preguntas = array_merge($pregunta["respuestas_correctas"], $pregunta["respuestas_incorrectas"]);
-            shuffle($mezcla_preguntas);
-            $Respuestas_mezcladas [] = $mezcla_preguntas;
-        }
+		file_put_contents(__DIR__.'/preguntas_ultimo_examen.yml', $Preguntas_de_Ultimo_Examen);
 
-        //Escribo las preguntas en un archivo yml para cuando genero las rtas
-        $Preguntas_de_Ultimo_Examen = Yaml :: dump ($Preguntas);
+		$Rtas_de_Ultimo_Examen = Yaml::dump($Respuestas_mezcladas);
 
-        file_put_contents(__DIR__.'/preguntas_ultimo_examen.yml' ,$Preguntas_de_Ultimo_Examen);
+		file_put_contents(__DIR__.'/rtas_ultimo_examen.yml', $Rtas_de_Ultimo_Examen);
 
-        //Lo mismo para las rtas
-        $Rtas_de_Ultimo_Examen = Yaml :: dump($Respuestas_mezcladas);
+    	return [$Preguntas, $Respuestas_mezcladas];
 
-        file_put_contents(__DIR__.'/rtas_ultimo_examen.yml', $Rtas_de_Ultimo_Examen);
-
-        return [$Preguntas, $Respuestas_mezcladas];
-        
     }
-    
+
     public function MostrarRtas(){
+    	
+    	if(file_exists( __DIR__.'/preguntas_ultimo_examen.yml') ){
+    		
+    		$Preguntas = $this->Parsear_Archivo("/preguntas_ultimo_examen.yml");
+    		$Respuestas = $this->Parsear_Archivo("/rtas_ultimo_examen.yml");
 
-        if (file_Exists (__DIR__.'/preguntas_ultimo_examen.yml') ){
-       
-            $Preguntas = $this->Parsear_Archivo("/preguntas_ultimo_examen.yml");
-    	    $Respuestas = $this->Parsear_Archivo("/rtas_ultimo_examen.yml");
-
-    	    return $this->render('default/MostrarRtas.html.twig', ["preguntas" => $Preguntas, "Respuestas_mezcladas" => $Respuestas]);
-        }
-        //No puedo mostrar las rtas de un examen que no se genero
-        else{
-            return $this->render('default/No_existe_examen.html.twig');
-        }
-
+    		return $this->render('default/MostrarRtas.html.twig', ["preguntas" => $Preguntas, "Respuestas_mezcladas" => $Respuestas]);
+    	}
+    	else{
+    		return $this->render('default/No_existe_examen.html.twig');
+    	}
+    	
     }
-
 
     public function index()
     {
-        $PreguntasyRtas_Mezcladas = $this->Mezclar_Preguntas();
-        $Preguntas = $PreguntasyRtas_Mezcladas[0];
-        $Respuestas_mezcladas = $PreguntasyRtas_Mezcladas[1];
-        
+    	$PreguntasyRtas_Mezcladas = $this->Mezaclar_Preguntas();
+    	$Preguntas = $PreguntasyRtas_Mezcladas[0];
+    	$Respuestas_mezcladas = $PreguntasyRtas_Mezcladas[1];
+    	//dump($Preguntas);
+
         return $this->render('default/index.html.twig', ["preguntas" => $Preguntas, "Respuestas_mezcladas" => $Respuestas_mezcladas]);
     }
 }
